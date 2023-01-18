14f2b6048aa0733afdb5d07482fcdbaa07c376b8
KC_Dev <burakkcelebi@hotmail.com>
2023-01-18 21:10:47 +0000

    Practiced! Drum Kit,Event Capture & Propagation...
    01 - JavaScript Drum Kit and 25 - Event Capture, Propagation, Bubbling & Once are practiced.

---
+++ 25 - Event Capture, Propagation, Bubbling and Once/index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const divs = document.querySelectorAll('div');
+
+function logText(e) {
+    console.log(this.classList.value);
+    e.stopPropagation(); // stop bubbling
+}
+
+divs.forEach(div => div.addEventListener('click', logText, {
+    capture: false
+}));
